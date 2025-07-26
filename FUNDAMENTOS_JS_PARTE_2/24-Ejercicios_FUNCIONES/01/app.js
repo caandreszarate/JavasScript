ba79ed7e2be717fc4e3f58cfaae378e2f8e97041
caandreszarate <caandreszarate@gmail.com>
2025-07-26 14:18:31 -0500

    PrimerosEjercicios

---
+++ FUNDAMENTOS_JS_PARTE_2/24-Ejercicios_FUNCIONES/01/app.js
@@ -0,0 +1,29 @@
+/*
+==============================================
+    EJERCICIO 1 - FUNCIONES
+==============================================
+
+*/
+
+function saludar(nombre){
+    return `Hola ${nombre}`;
+}
+
+//pruebas
+console.log(`Ejercicio 1 - Funcion que Saluda a un usuario`);
+console.log(saludar("Juan"));
+console.log(saludar("Maria"));
+console.log(saludar("Pedro"));
+
+function sumar(a, b){
+    return a + b;
+}
+
+//pruebas
+console.log(`Ejercicio 2 - Funcion que suma dos numeros`);
+console.log(sumar(1, 2));
+console.log(sumar(3, 4));
+console.log(sumar(5, 6));
+
+
+
